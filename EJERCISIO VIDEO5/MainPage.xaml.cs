bfcd35ad69b9d6568a46ea1f00d5036d825a882f
MINEDUCYT <MINEDUCYT@DESKTOP-OAO2BUA>
2023-06-24 15:16:05 -0600

    Agregar archivos de proyecto.

---
+++ EJERCISIO VIDEO5/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EJERCISIO_VIDEO5
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            boton.Text = "Presioname de nuevo";
+        }
+    }
+}
